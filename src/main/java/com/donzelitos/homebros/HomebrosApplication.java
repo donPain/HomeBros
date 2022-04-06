230c0abbb8199d1a2a476f2a99bff1cedc861d5b
= <eduardo.plis@hotmail.com>
2022-04-06 18:28:32 -0300

    Start project

---
+++ src/main/java/com/donzelitos/homebros/HomebrosApplication.java
@@ -0,0 +1,13 @@
+package com.donzelitos.homebros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donzelitos/homebros/HomebrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donzelitos.homebros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomebrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
